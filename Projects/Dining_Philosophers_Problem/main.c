c5dda3b9070338901fb67282122d1dcc9cd41897
austindecker <deckaj01@pfw.edu>
2024-03-26 13:41:13 -0400

    testing pthreads

--- Projects/Dining_Philosophers_Problem/main.c
+++ Projects/Dining_Philosophers_Problem/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 
+#define THREAD_COUNT 5
+
+
+void* worker_function(void* args){
+    int * id = (int*) args;
+    printf("thread id:%d\n", *id);
+    free(id);
+
+    return NULL;
+}
 
 void main (){
-    printf("hello world\n");
+    pthread_t threads[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
+        int *id = malloc(sizeof(int));
+        *id = i;
+        pthread_create(&threads[i], NULL, worker_function, (void *)id);
+    }
+
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    
 }
